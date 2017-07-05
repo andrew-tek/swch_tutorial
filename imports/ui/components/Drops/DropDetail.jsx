01a494795d439b08d73acfdc16d1204ad55d30e4
PirateKingDee <andyliang987@hotmail.com>
2017-07-05 14:17:15 -0700

    timer, login

--- imports/ui/components/Drops/DropDetail.jsx
+++ imports/ui/components/Drops/DropDetail.jsx
@@ -7,7 +7,9 @@ export default class DropDetail extends Component {
     //find out current tier
     //get needed participants from tier participants - current participants
     //display needed participants and next tier price
+
     let product = this.props.product;
+    let expireDay = product.end_date;
     let curTier = product.cur_tier;
     let curParticipants = product.participants;
     let tiers = product.tiers;
@@ -20,7 +22,7 @@ export default class DropDetail extends Component {
       nextPrice = nextTier.price;
     }
     let addParticipant = () => {
-      Meteor.call("addParticipant", product._id);
+      Meteor.call("addParticipants", product._id);
     }
     var Countdown = require('react-cntdwn');
     var handleFinish = function () {
@@ -42,9 +44,11 @@ export default class DropDetail extends Component {
               :<span > ${product.org_price} </span> }
               <div>{nextTier ? <span>{needParticipants} more people needed for this price ${nextPrice}</span>
               : <span></span>}</div>
-                <button onClick={addParticipant} className="btn btn-primary text-center">Participate Now</button>
-                <Countdown targetDate={new Date('July 7, 2017')}
-               startDelay={0}
+                {!!Meteor.userId() ?
+                  <button onClick={addParticipant} className="btn btn-primary text-center">Participate Now</button>
+                    : <Link to="/signin"><button className="btn btn-primary text-center">Participate Now(Lead to Signin)</button></Link>}
+                <Countdown targetDate={expireDay}
+                startDelay={0}
                interval={1000}
                timeSeparator={' : '}
                leadingZero
