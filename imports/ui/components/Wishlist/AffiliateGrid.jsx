5d2c68bcc3f2ba0b89030e8aa1f7203c9a1dc0d5
macheather <heatherlinmac@gmail.com>
2017-08-08 22:11:06 -0700

    one

--- imports/ui/components/Wishlist/AffiliateGrid.jsx
+++ imports/ui/components/Wishlist/AffiliateGrid.jsx
@@ -9,9 +9,9 @@ export default class AffiliateGrid extends Component {
       <div className="col-sm-3">
         <img className="img-responsive"   src={affiliate.image}/>
         <h2 className="text-center"> {affiliate.product_name}  </h2>
-        <h2 className="text-center"> Purchased by: {affiliate.buyer_username}  </h2>
+        <h8 className="text-center"> Purchased by: {affiliate.buyer_username}  </h8>
         <div className="text-center">
-          <h2> <span>{accounting.formatMoney(affiliate.affiliate_earned)} </span> </h2>
+          <h8> <span> Earned: {accounting.formatMoney(affiliate.affiliate_earned)} </span> </h8>
         </div>
       </div>
     )
