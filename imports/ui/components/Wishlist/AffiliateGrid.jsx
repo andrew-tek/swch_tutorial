2a92a8eb6e3683d0a72a63a3e5fbc5d8eecb1251
macheather <heatherlinmac@gmail.com>
2017-08-09 10:31:03 -0700

    UI

--- imports/ui/components/Wishlist/AffiliateGrid.jsx
+++ imports/ui/components/Wishlist/AffiliateGrid.jsx
@@ -6,14 +6,12 @@ export default class AffiliateGrid extends Component {
   render() {
     let affiliate = this.props.affiliate;
     return (
-      <div className="col-sm-3">
-        <img className="img-responsive"   src={affiliate.image}/>
-        <h2 className="text-center"> {affiliate.product_name}  </h2>
-        <h8 className="text-center"> Purchased by: {affiliate.buyer_username}  </h8>
-        <div className="text-center">
-          <h8> <span> Earned: {accounting.formatMoney(affiliate.affiliate_earned)} </span> </h8>
-        </div>
-      </div>
+
+      <tr>
+        <td>{affiliate.product_name}</td>
+        <td>{affiliate.buyer_username}</td>
+        <td>{accounting.formatMoney(affiliate.affiliate_earned)}</td>
+      </tr>
     )
   }
 }
